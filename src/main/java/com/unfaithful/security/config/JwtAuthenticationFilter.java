da902b3cf7e5dd65d255dbd70a87d50efd7153a5
Gürkan Gökmen <122023578+gurkangokmen@users.noreply.github.com>
2024-12-13 19:23:11 +0300

    jwt authfilter && jwt validation

---
+++ src/main/java/com/unfaithful/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package com.unfaithful.security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+// Run at every request
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return; //We don't want to continue if there is no token
+        }
+
+        jwt = authHeader.substring(7);
+
+        // Extract the email from the token
+        userEmail = jwtService.extractUsername(jwt);
+
+    }
+}
